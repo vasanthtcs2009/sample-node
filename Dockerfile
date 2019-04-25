3f646756eef589f51b7174afe8e2456ba2643d75
vasanthtcs2009 <vasanthtcs2009@gmail.com>
2019-04-25 13:31:26 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#to run node
+FROM node:alpine
+
+USER root
+
+#set working directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+
+#install npm dependencies
+COPY package*.json ./
+RUN npm install
+
+#copy all the code
+COPY . .
+
+#start the app
+EXPOSE 2102
+CMD [ "npm" , "start" ]
---
+++ hello-world.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const https = require('https');
+
+var index = express();
+
+const port = 2102;
+
+//adding middleware
+index.use(cors());
+
+
+//defining default routes
+index.use('/',(req,res)=>{
+    res.send({
+        responseCode: '0000',
+        responseMessage: 'Success',
+        responseDescription:'Test Page, Application Boot successful!!'
+    });
+})
+
+//start the app on importing
+index.listen(port,()=>{
+  console.log('Port started successfully on : '+port);
+})
